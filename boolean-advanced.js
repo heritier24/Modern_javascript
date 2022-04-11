43aab7f0cecd76c0e6f9c7efa2ec703d7b01b322
heritier24 <heritier.ganza@kudibooks.com>
2022-04-11 18:59:52 +0200

    boolean advanced

---
+++ boolean-advanced.js
@@ -0,0 +1,14 @@
+
+
+// challenge if and else satatement boolean 
+
+let temp = 45 
+
+if(temp <= 32){
+
+    console.log("it is freezing outside ");
+}else if( temp >=120 ){
+    console.log("it is hot outside !");
+}else {
+    console.log("Go for it. it is pretty nice ");
+}
