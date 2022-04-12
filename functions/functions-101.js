b19dd402f577586f42db213cc571e1b83048145a
heritier24 <heritier.ganza@kudibooks.com>
2022-04-12 06:14:19 +0200

    added function 101 js with an example of challenge to convert fahreneit to celcius

---
+++ functions/functions-101.js
@@ -0,0 +1,30 @@
+
+
+// Functions - input, code, output 
+
+
+let square = function(num){
+
+    let result = num * num 
+    return result;
+}
+
+let value = square(3);
+let othervalue = square(10);
+console.log(value);
+console.log(othervalue);
+
+// challenge area 
+
+// convert fahrenheit to celcius 
+// call a couple of time (32->0) (68->20)
+// print the converted values 
+let fahrenheit = function(value){
+    let celcius = (value -32) * 5/9
+    return celcius;
+}
+let tempOne = fahrenheit(32);
+let tempTwo = fahrenheit(68);
+
+console.log(tempOne);
+console.log(tempTwo);
