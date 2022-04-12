06a992d63e529c4f994f55de46a614dbe80df374
heritier24 <heritier.ganza@kudibooks.com>
2022-04-12 16:03:58 +0200

    added section of functions with arguments

---
+++ functions/arguments.js
@@ -0,0 +1,28 @@
+
+// Multiple arguments 
+let add = function(a,b,c){
+    return a + b + c ;
+}
+
+let result = add(5, 8, 2);
+
+console.log(result);
+
+// Default arguments 
+
+let getScoreText = function(name= "Heritier", score = 40){
+    return 'name:' +name + "  score : " + score
+}
+
+let scoreText = getScoreText();
+console.log(scoreText);
+
+// challenge area 
+
+// * 2 people go to the restaurant then after payment ask the tip for the 20 % of the bills paid
+
+let getTip = function (bills = 100, tip = 20) {
+    return (bills * tip) /100
+}
+let tipAmount = getTip();
+console.log(tipAmount);
