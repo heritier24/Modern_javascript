1dcb369ccee175dab22776c4d04f61f0e6c644be
heritier24 <heritier.ganza@kudibooks.com>
2022-04-13 08:48:27 +0200

    added object section contain a challenge

---
+++ objects/objects-101.js
@@ -0,0 +1,18 @@
+
+// challenge 
+
+// create an object have name, age and location 
+
+// print the message andrew is 25 years old and live in kigali 
+
+// second increase the age by 1 and print the message again 
+
+let myPerson = {
+    myName: 'Heritier',
+    age: 25,
+    location: 'kigali'
+}
+
+console.log(`${myPerson.myName} is ${myPerson.age} years old and live in ${myPerson.location}`)
+
+console.log(`${myPerson.myName} is ${myPerson.age + 1} years old and live in ${myPerson.location}`)
