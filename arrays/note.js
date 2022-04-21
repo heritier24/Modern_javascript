5746eac308bb0ffebd2957b9f8d8cea2430bc79e
heritier24 <heritier.ganza@kudibooks.com>
2022-04-21 08:35:51 +0200

    note array

--- arrays/note.js
+++ arrays/note.js
@@ -2,4 +2,6 @@
 const notes = ["note 1", "note 2 ", "note 3"];
 
 console.log(notes.length);
-console.log(notes[notes.length -2])
+notes[2] = "this is the new note 3"
+console.log(notes.length);
+console.log(notes)
