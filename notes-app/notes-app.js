09484d00210903e2853125fe62f1451e9c98e689
heritier24 <heritier.ganza@kudibooks.com>
2022-04-27 09:55:10 +0200

    handling user interaction

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -7,6 +7,8 @@ let p = document.querySelector('p')
 let ps = document.querySelector('button', function(){
     console.log("is this working ");
 });
-document.querySelector('button').addEventListener('click', function(){
+document.querySelector('button').addEventListener('click', function(e){
     console.log("is this working !");
+    e.target.textContent = 'The button Was Clicked '
+    console.log('add Element in the paragraph ')
 })
