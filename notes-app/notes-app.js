b8804f2f00972d6aee114143a5b0dad41f1d940c
heritier24 <heritier.ganza@kudibooks.com>
2022-04-27 18:34:53 +0200

    checkbox queryselector event

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,24 +1,9 @@
 
 
-let p = document.querySelector('p')
-
-// console.log(p)
-
-let ps = document.querySelector('button', function(){
-    console.log("is this working ");
-});
-document.querySelector('#create-note').addEventListener('click', function(e){
-    console.log("is this working !");
-    e.target.textContent = 'The button Was Clicked '
-    document.querySelectorAll('.note').forEach(function(note){
-        note.textContent = 'Created new note for each paragraph and the button was clicked '
-    })
-})
-document.querySelector('#remove-note').addEventListener('click',function(e){
-    document.querySelectorAll('.note').forEach(function(note){
-        note.remove();
-    })
-})
-document.querySelector('#search-text').addEventListener('input', function(event){
-    console.log(event.target.value)
+const filters = {
+    searchText: '',
+    hideCompleted: false
+}
+document.querySelector('#for-fun').addEventListener('input', function (e) {
+    console.log(e.target.checked)
 })
