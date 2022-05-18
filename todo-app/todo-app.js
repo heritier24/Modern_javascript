1759143b6c24ff87454bfa5bdb92c71dc396218f
heritier24 <heritier.ganza@kudibooks.com>
2022-05-18 11:13:14 +0200

    package modified and todo-app.js

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -43,6 +43,9 @@ const todos = [{
     completed: true
 }
 ]
+const seearching = {
+    searchText: ''
+}
 
 const incompleteTodos = todos.filter(function (todo){
     return !todo.completed
@@ -56,4 +59,68 @@ todos.forEach(function (todo){
     const p = document.createElement('p')
     p.textContent = todo.text
     document.querySelector('body').appendChild(p)
+})
+
+const renderSearch = function(todos, seearching){
+    const filterTodos = todos.filter(function(todo){
+        return todo.text.includes(seearching.searchText)
+    })
+    document.querySelector('#notes').innerHTML = '';
+    filterTodos.forEach(function(todo){
+        const todoEl = document.createElement('p')
+        todoEl.textContent = todo.text
+        document.querySelector('#notes').appendChild(todoEl)
+    })
+}
+renderSearch(todos, seearching)
+
+// search todo 
+document.querySelector('#search-todo').addEventListener('input', function (e){
+    seearching.searchText = e.target.value
+    renderSearch(todos, seearching)
+})
+
+
+
+document.querySelector('#todo-text').addEventListener('change', function(e){
+    e.preventDefault();
+    console.log(e.target.value)
+})
+
+const notes = [{
+    title: "checmistry",
+    body: "chemistry note "
+},
+{
+    title: "biology",
+    body: "biology note"
+},
+{
+    title: "mathematics",
+    body: "mathematics notes"
+}
+]
+
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function(notes, filters){
+    const filteredNotes = notes.filter(function (note){
+        return note.title.includes(filters.searchText)
+    })
+
+    console.log(filteredNotes)
+    // filteredNotes.forEach(function (note){
+    //     const notEl = document.createElement('p')
+    //     notEl.textContent = note.title
+    //     document.querySelector('body').appendChild(notEl)
+    // })
+}
+renderNotes(notes, filters)
+
+document.querySelector('#search-text').addEventListener('input', function(e){
+    filters.searchText = e.target.value
+    renderNotes(notes,filters)
+    console.log("ok")
 })
