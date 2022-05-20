a67a8c35ae62bf67af961f37662ed42e54abe0cd
heritier24 <heritier.ganza@kudibooks.com>
2022-05-20 13:37:12 +0200

    ternary condition operator with array

--- advanced/condition-operator.js
+++ advanced/condition-operator.js
@@ -1,6 +1,25 @@
 
-const myAge = 20
+// const myAge = 20
 
-const message = myAge > 18 ? 'you can vote !' : ' you can not vote. '
+// const message = myAge > 18 ? 'you can vote !' : ' you can not vote. '
 
-console.log(message)
+// console.log(message)
+
+const myAge = 20 
+
+const showPage = () => {
+    console.log("showing the page ");
+}
+
+const errorPage = () => {
+    console.log("showing the error page ");
+}
+
+myAge >=20 ? showPage() : errorPage()
+
+
+
+
+const team = ["kudibooks", "academic bridge"]
+
+console.log(team.length <= 4 ? `team size : ${team.length}` : 'Too many people on your team')
